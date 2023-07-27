f21dfaf2cb3804575a01c82d5cd366b20d1beb95
Frank <fobellofrank@gmail.com>
2023-07-26 23:16:44 -0300

    Menu de navegacion agregado

--- mi-portfolio/src/App.jsx
+++ mi-portfolio/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
+function Nav() {
+  return(
+    <>
+      <header>
+        <nav>
+          <h2>Frank</h2>
+        </nav>
+        <div className="menu-options-mobile">
+                  <button><img src="./img/home.png" alt="" class="icon"/></button>
+                  <button class="aboutMe"><img src="./img/user.png" alt="" class="icon"/></button>
+                  <button class="exp"><img src="./img/support.png" alt="" class="icon"/></button>
+                  <button><img src="./img/linkedin.png" alt="" class="icon"/></button>
+          </div>
+      </header>  
+    </>
+  )
+}
 
 function App() {
   
   return (
     <>
-      
+      <Nav/>
     </>
+      
   )
 }
 
