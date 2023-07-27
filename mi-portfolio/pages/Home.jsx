36ba2b1699e0451200b49b9547b31369fcfb6eed
Frank <fobellofrank@gmail.com>
2023-07-27 20:12:13 -0300

    Seccion home terminada y fondo gradiente animado agregado

--- mi-portfolio/pages/Home.jsx
+++ mi-portfolio/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import '../src/styles.css'
+import '../styles/Home.css'
 const Home = () => {
   return (
-    <div>
-        <h1>
-            Bienvenido a mi portfolio :D
-        </h1>
+    <div className='bodyHome'>
+      <div className='card'>
+        <img src="../public/img/profile.jpg" alt="" className='profile' />
+        <div className='textContainer'>
+          <h3 className='textHome'>Hola! Bienvenido a mi portfolio, mi nombre es Frank y soy desarrollador full-stack.</h3>
+        </div>
+          
+      </div>
     </div>
   )
 }
--- mi-portfolio/pages/Nav.jsx
+++ mi-portfolio/pages/Nav.jsx
@@ -7,10 +7,10 @@ const Nav = () => {
             <h2>Frank</h2>
         </nav>
         <div className="menu-options-mobile">
-            <button><img src="./img/home.png" alt="" class="icon"/></button>
-            <button class="aboutMe"><img src="./img/user.png" alt="" class="icon"/></button>
-            <button class="exp"><img src="./img/support.png" alt="" class="icon"/></button>
-            <button><img src="./img/linkedin.png" alt="" class="icon"/></button>
+            <button><img src="./img/home.png" alt="" className="icon"/></button>
+            <button className="aboutMe"><img src="./img/user.png" alt="" className="icon"/></button>
+            <button className="exp"><img src="./img/support.png" alt="" className="icon"/></button>
+            <button><img src="./img/linkedin.png" alt="" className="icon"/></button>
         </div>
     </header>
   )
